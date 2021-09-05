37afa6d66c52f81442a4b89eb2a60ba463f5a8cb
Jonas Magalhães <jonas@easycomtec.com>
2021-09-05 00:24:11 -0300

    Added conditional logged ou not logged

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from 'next';
 
 import LoginPage from "./app/pages/login/LoginPage";
+import HomePage from './app/pages/home/HomePage';
 
 const Home: NextPage = () => {
-  return (
-    <LoginPage/>
-  )
+  const isLogged: boolean = true;
+
+  if(isLogged) {
+    return (
+      <HomePage />
+    )
+  } else {
+    return (
+      <LoginPage />
+    )
+  }
 }
 
 export default Home

63626e50791d791b1d6eed7f45eb95866da731ad
Jonas Magalhães <jonas@easycomtec.com>
2021-09-04 22:22:29 -0300

    Added tailwind

---
+++ pages/app/pages/login/LoginPage.tsx
@@ -0,0 +1,9 @@
+import CardLogin from '../../components/CardLogin';
+
+export default function LoginPage() {
+    return (
+        <div className="bg-blue-400 grid h-screen place-items-center">
+            <CardLogin/>
+        </div>
+    )
+}
---
+++ pages/app/pages/register/RegisterPage.tsx
@@ -0,0 +1,10 @@
+// import CardLogin from "../../components/CardLogin";
+
+export default function RegisterPage() {
+    return (
+        <div className="bg-blue-400 grid h-screen place-items-center">
+            <p> Teste </p>
+            {/* <CardLogin/> */}
+        </div>
+    )
+}

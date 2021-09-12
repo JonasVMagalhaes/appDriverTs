1e9929111d2dda88cd79016bb477a5af4db6ff3c
Jonas Magalhães <jonas@easycomtec.com>
2021-09-11 21:01:35 -0300

    Added controller message

--- src/pages/api/controllers/messageController.ts
+++ src/pages/api/controllers/messageController.ts
@@ -2,8 +2,12 @@ import { NextApiRequest } from "next";
 
 import IResponse from "../interfaces/IResponse";
 
+import { sendMessage } from "../services/messageService";
+
 export default async function messageController(req: NextApiRequest): Promise<IResponse> {
     switch (req.method) {
+        case "POST": return await sendMessage(req);
+
         default: return await notMethodFinded(req);
     }
 }
---
+++ src/pages/api/services/messageService.ts
@@ -0,0 +1,6 @@
+import IResponse from "../interfaces/IResponse";
+
+export async function sendMessage(credentials: any): Promise<IResponse> {
+    return { status: 200, response: "" }
+}
+

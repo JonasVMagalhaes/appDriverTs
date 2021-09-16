2a24f853d79ff1c26223fdb68abbcf67343e3d94
Jonas Magalhães <jonas@easycomtec.com>
2021-09-15 23:27:42 -0300

    Last updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,10 @@
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
-
-import { firebaseCloudMessaging } from './api/utils/firebase'
 
 import 'tailwindcss/tailwind.css';
 import '@styles/global.css';
-import '@styles/chat.style.css'
+import '@styles/chat.style.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-
-  useEffect(() => {
-    firebaseCloudMessaging.init()
-  })
 
   return (
     <>
